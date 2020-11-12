1063b6b0c912f50de8ac30e7a971cd2d0a2d5c7f
Felipe Moya <felipe.moya@uc.cl>
2020-11-12 20:44:53 -0300

    avance readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# LET0010
+# Proyecto LET0010 Segundo Semestre 2020
+
+*codigo*  
+En esta carpeta se encuentra el código para explorar las distintas opciones de análisis que se pueden hacer con estos los datos-procesados. Además proximamente se encontrará el código que generará los gráficos y con el que se hará el análisis final de estos datos.
+
+*datos-procesados*  
+Aquí se encuentran 5 archivos csv, donde cada uno tiene toda la información sobre cada partido disputado en la premier league de Inglaterra en la temporada respectiva al nombre del archivo. Además se encuentra el archivo __notes.txt__, donde se muestra la info de cada variable de la base de datos, donde las variables son iguales para cada temporada.
+Estos datos fueron extraídos de la siguiente página [Click Aquí][https://www.football-data.co.uk/englandm.php].
+
+*datos-sin-procesar*
+
+*figuras*
+
+*productos*
--- codigos/analisis-datos.R
+++ codigos/analisis-datos.R
@@ -1,23 +1,24 @@
 library(rio)
 library(ggplot2)
 library(lubridate)
+library(readr)
 
-pl_19_20 = rio::import("pl 2019-2020.csv")
+pl_19_20 = rio::import("datos-sin-procesar/pl 2019-2020.csv")
 pl_19_20 = pl_19_20[1:24]
 
-pl_18_19 = rio::import("pl 2018-2019.csv")
+pl_18_19 = rio::import("datos-sin-procesar/pl 2018-2019.csv")
 pl_18_19 = pl_18_19[1:23]
 
-pl_17_18 = rio::import("pl 2017-2018.csv")
+pl_17_18 = rio::import("datos-sin-procesar/pl 2017-2018.csv")
 pl_17_18 = pl_17_18[1:23]
 
-pl_16_17 = rio::import("pl 2016-2017.csv")
+pl_16_17 = rio::import("datos-sin-procesar/pl 2016-2017.csv")
 pl_16_17 = pl_16_17[1:23]
 
-pl_15_16 = rio::import("pl 2015-2016.csv")
+pl_15_16 = rio::import("datos-sin-procesar/pl 2015-2016.csv")
 pl_15_16 = pl_15_16[1:23]
 
-pl_14_15 = rio::import("pl 2014-2015.csv")
+pl_14_15 = rio::import("datos-sin-procesar/pl 2014-2015.csv")
 pl_14_15 = pl_14_15[1:23]
 
 # 19-20
@@ -345,10 +346,12 @@ if (pjv_14_15[which(pjv_14_15$HomeTeam == "Tottenham"),]$FTR == "A") {
   victorias_14_15 = victorias_14_15 + 1
 }
 
-
-
-
-
+# Escrbimos los datos de cada temporada, sin los datos sobre apuestas
+write_csv(pl_14_15, here::here("datos-procesados", "pl_14_15.csv"))
+write_csv(pl_15_16, here::here("datos-procesados", "pl_15_16.csv"))
+write_csv(pl_16_17, here::here("datos-procesados", "pl_16_17.csv"))
+write_csv(pl_18_19, here::here("datos-procesados", "pl_18_19.csv"))
+write_csv(pl_19_20, here::here("datos-procesados", "pl_19_20.csv"))
 
 
 
